051c5a1b2ee58b115743cfc56286e5f485ffdb58
Leandro Pestana <leandrofap@gmail.com>
2021-11-14 19:44:04 -0300

    First
    Estutura Base

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <!-- https://cdnjs.com/libraries/twitter-bootstrap/5.0.0-beta1 -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css"
+    />
+
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://use.typekit.net/bgm1dgn.css">
+
+    <!-- Icons: https://getbootstrap.com/docs/5.0/extend/icons/ -->
+    <!-- https://cdnjs.com/libraries/font-awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+    />
+
+    <title>Engov - Vem Bonde</title>
+  </head>
+  <body class="vw-100 vh-100 home">
+
+  <section class="container-fluid banner-home d-flex justify-content-center">
+      <article class="d-flex flex-column">
+          <img class="rounded mx-auto d-block" src="assets/images/logo-bonde-da-curticao-engov.png " alt="">
+          <div class="clip">
+             <img src="assets/images/video.svg" alt="Clip Engov Vem Pro Bonde">
+          </div>
+      </article>
+  </section>
+
+  <section class="container-fluid vem-pro-bonde">
+      <article class="row d-flex justify-content-center">
+            <div class="col-md-6 d-flex flex-column ">
+                <h1 class="text-center">SE VOCÊ É DOS GAMES, DAS FESTAS, DOS SHOWS, VEM PRO BONDE</h1>
+                <img src="assets/images/raio.svg" alt="raio engov">
+            </div>
+      </article>
+  </section>
+
+  <section class="conteiner-fluid engov-after-home d-flex justify-content-center">
+        <article class="row d-flex flex-column">
+            <div>
+                <img src="assets/images/Videoleap-6FFA0426-4FF6-4202-A2FD-113D22F049DC-BAIXA_PosterImage.png" alt="">
+            </div>
+         </article>
+  </section>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
+</html>
