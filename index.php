74e6c314202fa03c86ad6f820fe9bdafec9e5edf
Leandro Pestana <leandrofap@gmail.com>
2021-11-14 23:07:53 -0300

    Box Engov e Footer

--- index.php
+++ index.php
@@ -26,20 +26,27 @@
   </head>
   <body class="vw-100 vh-100 home">
 
-  <section class="container-fluid banner-home d-flex justify-content-center">
-      <article class="d-flex flex-column">
-          <img class="rounded mx-auto d-block" src="assets/images/logo-bonde-da-curticao-engov.png " alt="">
-          <div class="clip">
-             <img src="assets/images/video.svg" alt="Clip Engov Vem Pro Bonde">
+  <section class="container-fluid banner-home">
+      <article class="row d-flex justify-content-center">
+          <div class="col-md-8">
+              <img class="rounded mx-auto d-block" src="assets/images/logo-bonde-da-curticao-engov.png " alt="">
           </div>
+
+          <div class=" col-md-10 embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5ALlpUN4cbc?rel=0" allowfullscreen></iframe>
+        </div>       
+
       </article>
   </section>
+
+
+  
 
   <section class="container-fluid vem-pro-bonde">
       <article class="row d-flex justify-content-center">
             <div class="col-md-6 d-flex flex-column ">
                 <h1 class="text-center">SE VOCÊ É DOS GAMES, DAS FESTAS, DOS SHOWS, VEM PRO BONDE</h1>
-                <img src="assets/images/raio.svg" alt="raio engov">
+                <img class="rounded mx-auto d-block" src="assets/images/raio.svg" alt="raio engov">
             </div>
       </article>
   </section>
@@ -48,7 +55,7 @@
         <article class="row d-flex justify-content-center" >
             <div class="d-flex flex-column col-md-10">
                 <img src="assets/images/poca-engov-after.mp4" alt="">
-                <img class="logo-engov-after" src="assets/images/logo-engov-after.svg" alt="Engov After">
+                <img class="logo-engov-after rounded mx-auto d-block" src="assets/images/logo-engov-after.svg" alt="Engov After">
             </div>
          </article>
          <article class="row d-flex justify-content-center">
@@ -63,6 +70,85 @@
   </section>
 
 
+  <section class="engov-home conteiner-fluid ">
+        <article class="row d-flex justify-content-center" >
+            <div class="d-flex flex-column col-md-10">
+                <img src="assets/images/poca-engov-after.mp4" alt="">
+            </div>
+         </article>
+         <article class="row d-flex justify-content-center">
+            <div class="d-flex flex-column col-md-10 box-produto">
+                <img class="rounded mx-auto d-block" src="assets/images/caixa-engov-2.png" alt="Engov After">
+                <h2 class="text-center">E TOME CURTIÇÃO</h2>
+                <div class="d-flex justify-content-center">
+                    <button>CONHEÇA</button>
+                </div>
+            </div>
+         </article>
+  </section>
+
+
+  <section class="container-fluid canais-home">
+    <article class="row d-flex justify-content-center">
+        <div class="col-md-6 d-flex flex-column">
+            <h3 class="text-center">ACOMPANHE NOSSOS CANAIS<br/> E FIQUE POR DENTRO DE TUDO</h3>
+            <ul class="d-flex justify-content-evenly" >
+                <li><img src="assets/images/instagram-icon.svg" alt=""></li>
+                <li><img src="assets/images/facebook-icon.svg" alt=""></li>
+                <li><img src="assets/images/tiktok-icon.svg" alt=""></li>
+                <li><img src="assets/images/youtube-icon.svg" alt=""></li>
+                <li><img src="assets/images/spotify-icon.svg" alt=""></li>
+            </ul>
+        </div>
+    </article>
+
+    <article class="row d-flex justify-content-center">
+        <div class="col-md-6">
+             <!-- LightWidget WIDGET -->
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/3a38162f8c8b5109b580acbaf45d9f6f.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+        </div>
+    </article>
+  </section>
+
+ 
+<footer class="container-fluid">
+    <article class="row d-flex justify-content-center">
+        <div class="col-md-8 d-flex flex-column">
+            <ul class="d-flex justify-content-md-between" >
+                <li>CONTATO</li>
+                <li>TERMOS DE USO</li>
+                <li>POLÍTICA DE PRIVACIDADE</li>
+                <li>ASSESSORIA DE IMPRENSA</li>
+            </ul>
+        </div>
+    </article>
+    <article class="row d-flex justify-content-center">
+        <div  class="col-md-8 d-flex flex-column">
+            <img class="rounded mx-auto d-block mt-5 pt-5" src="assets/images/logo-bonde-da-curticao-engov.png" alt="">
+        </div>
+    </article>
+
+    <article class="row d-flex justify-content-center">
+        <div class="col-md-8 d-flex flex-column mt-5 pt-5">
+            <p>
+            Engov After: Alimento isento de registro conforme RDC 27/2010. *Recupera eletrólitos e líquidos perdidos durante a atividade intensa. **Citrato trissódico dihidratado e cloreto de sódio: fontes de sódio. ***Glicose, carboidrato fonte de energia. </p>
+
+            <p>
+            Engov After: Ingredientes: água, sacarose, maltodextrina, glicose monohidratada, citrato trissódico dihidratado, cloreto de sódio, cafeína, aditivos1. ALÉRGICOS: CONTÉM TRIGO, CENTEIO, CEVADA E AVEIA E PODE CONTER CRUSTÁCEOS, OVOS, PEIXES, AMENDOIM, SOJA, LEITES, AMÊNDOAS, AVELÃS, CASTANHA-DE-CAJU, CASTANHA-DO-PARÁ, MACADÂMIAS, NOZES, PECÃS, PISTACHES, PINOLI E CASTANHAS. CONTÉM GLÚTEN. 1Tangerina: regulador de acidez: ácido cítrico, aromatizantes, acidulante: ácido tartárico, conservadores: sorbato de potássio e benzoato de sódio, edulcorantes: acesulfame de potássio e sucralose e corante: amarelo crepúsculo. Citrus: aromatizante, regulador de acidez: ácido cítrico, acidulante: ácido tartárico, conservadores: sorbato de potássio e benzoato de sódio e edulcorantes: acesulfame de potássio e sucralose. Berry Vibes: regulador de acidez: ácido cítrico, acidulante: ácido tartárico, aromatizantes, conservadores: sorbato de potássio e benzoato de sódio, edulcorantes: acesulfame de potássio e sucralose e corante: azul brilhante FCF. Red Hits: aromatizantes, regulador de acidez: ácido cítrico, acidulante: ácido tartárico, conservadores: sorbato de potássio e benzoato de sódio, corante: vermelho 40 e edulcorantes: acesulfame de potássio e sucralose. 
+            </p>
+            <p>
+            Engov. maleato de mepiramina, hidróxido de alumínio, ácido acetilsalicílico e cafeína. Indicação: alívio dos sintomas de dores de cabeça e alergias. MS 1.7817.0093. 
+            </p>
+
+            <p class="dengue">ESSE MEDICAMENTO É CONTRAINDICADO EM CASOS DE SUSPEITA DE DENGUE. SE PERSISTIREM OS SINTOMAS O MÉDICO DEVERÁ SER CONSULTADO. Fevereiro/2021.</p>
+        </div>
+    </article>
+</footer>
+
+
+
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/superembed.min.js"></script>
 </html>
